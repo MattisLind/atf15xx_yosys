bee1b4227c5338b6a0938ede06fa3324895228fe
Mattis Lind <mattis@mattisborgen.se>
2024-04-25 21:09:28 +0200

    Update run_fitter.sh

--- run_fitter.sh
+++ run_fitter.sh
@@ -90,7 +90,7 @@ echo Options = ${OPTS}
 
 FITTER=fit${DEVICE//[!0-9]/}.exe
 
-grep '--PIN:' ${NAME}.vhdl | cut -c7- > ${NAME}.pin
+grep "\-\-PIN:" ATF1508.vhdl | cut -c7- > ${NAME}.pin
 
 rm -f ${NAME}.fit
 
